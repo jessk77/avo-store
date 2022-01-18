a09ca032b945ec363e813f3f5dcb7eef52d0c57d
Jessica Karen Verduzco Ruiz <jessica.verduzco@improving.com>
2022-01-17 18:44:12 -0600

    API integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({data, length}) => setProductList(data))
+  }, [])
+
   return (
     <div>
       <Navbar />
       <div>Next.js!</div>
+      {
+        productList.map(product => 
+          <div key={product.id}>{product.id} {product.name}</div>
+        )
+      }
     </div>
   )
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
   const { query } = useRouter()
+  const [product, setProduct] = useState<TProduct>();
+
+  useEffect(() => {
+    console.log(query.id)
+    window
+      .fetch(`/api/avo/${query.id}`)
+      .then((response) => response.json())
+      .then((data) => setProduct(data))
+  }, [])
 
   return (
     <section>
       <h1>Página producto: {query.id}</h1>
+      {product && <p>Avo: {product.name}</p>}
     </section>
   )
 }
